30db447f456f13f18099ff14836ab70f181baac1
Sue Ceklosky <cekloskys@chc.edu>
2022-12-21 09:41:31 -0500

    End of Fall 2022 Semester

---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ContactsScreen from '../screens/Contacts';
+import FamilyContactsScreen from '../screens/FamilyContacts';
+import FriendContactsScreen from '../screens/FriendContacts';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = props => {
+
+  return (
+    <Tab.Navigator
+        screenOptions={{
+            tabBarActiveTintColor: 'blue',
+            tabBarLabelStyle: {
+                flex: 1,
+                fontSize: 15,
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 12,
+            },
+            tabBarStyle: {display: 'flex'},
+            tabBarIconStyle: {display: 'none'},
+        }}
+    >
+        <Tab.Screen name={'All'} component={ContactsScreen}/>
+        <Tab.Screen name={'Family'} component={FamilyContactsScreen}/>
+        <Tab.Screen name={'Friends'} component={FriendContactsScreen}/>
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
